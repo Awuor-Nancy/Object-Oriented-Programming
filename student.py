eebb49d54288114511d3421f5ddb67704e84f3d5
SylviahM <sylviahcheroposailenyi@gmail.com>
2022-05-27 19:26:30 +0300

    initial changes

--- student.py
+++ student.py
@@ -7,8 +7,14 @@ class Student:
         self.birth_year = birth_year
 
     def full_name(self):
-        self.birth_year
-        return "f hello {name},{Lname} you were born in {birth_year}"
+        return "f hello {name}, your last name is{Lname} "
+
+    def birth_year(self):
+         return "f hello {name},{Lname} you were born in {birth_year}"
+
+    def initials(self):
+        return "f Hi your initials are {name[0]} and {Lname[0]}"
+        
 
 
 
